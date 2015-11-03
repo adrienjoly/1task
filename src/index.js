f9fe64f23b93bacc0caa855d45e5b01ce774b497
Adrien Joly <adrien@whyd.com>
2015-11-03 15:54:43 +0700

    now loading options from Parse DB => added loading animation, and transition

--- src/index.js
+++ src/index.js
@@ -7,10 +7,30 @@
 
   // TODO: minify compiled bundle (e.g. use uglify), for production
 
+  // ___
+  // DB code
+
+  Parse.initialize("HAvVzC6nFUCQDskxkOio2sdiFNuWNGi9wgmX6Nwa", "jShePeIRlyKRj4S7lQ7uuktGEQn30b4DZxX7K1pb");
+  var Item = Parse.Object.extend('Item');
+
+  function storeItem(item) {
+    new Item().save(item);//.then(callback);
+    return item;
+  }
+
+  function storeDefaultItems() {
+    return [
+      { name: 'Tasks accumulate too much' },
+      { name: 'I don\'t know where to start' },
+      { name: 'I keep postponing my deadlines' }
+    ].map(storeItem);
+  }
+
+  // ___
+  // UI code
+
   // Needed for React Developer Tools
   window.React = React;
-
-  var appDiv = document.getElementById('app');
 
   function getSelectedItems(form) {
     var selected = [];
@@ -22,27 +42,43 @@
     return selected;
   }
 
-  var pollForm = React.createElement(PollForm, {
-    options: [
-      { name: 'Tasks accumulate too much' },
-      { name: 'I don\'t know where to start' },
-      { name: 'I keep postponing my deadlines' }
-      // TODO: persist user-generated problems, or re-use by other users
-    ],
-    onValidSubmit: function onSubmit() {
-      var form = document.getElementsByTagName('form')[0];
-      document.getElementById('js-merged-problems').value = getSelectedItems(form).join('\n');
-      form.submit();
-      /* AJAX code for testing with devtools' network tab:
-      var xhr = new XMLHttpRequest;
-      xhr.open('POST', '/', true);
-      xhr.send(new FormData(form));
-      */
+  function renderApp(options) {
+    console.log('renderApp with options:', options);
+    var element = React.createElement(PollForm, {
+      options: options || [],
+      onValidSubmit: function onSubmit() {
+        var form = document.getElementsByTagName('form')[0];
+        document.getElementById('js-merged-problems').value = getSelectedItems(form).join('\n');
+        form.submit();
+        /* AJAX code for testing with devtools' network tab:
+        var xhr = new XMLHttpRequest;
+        xhr.open('POST', '/', true);
+        xhr.send(new FormData(form));
+        */
+      }
+    });
+    var appDiv = document.getElementById('app');
+    return ReactDOM.render(element, appDiv, function() {
+      document.body.className = document.body.className.replace('is-loading', '');
+      appDiv.style.maxHeight = appDiv.childNodes[0].clientHeight + 'px';
+    });
+  }
+
+  // ___
+  // Main logic
+
+  console.log('Fetching options...');
+  new Parse.Query(Item).find({
+    success: function(items) {
+      items = items.map(function(item){
+        return { name: item.name || item.get('name') };
+      });
+      renderApp(!items.length ? storeDefaultItems() : items);
+    },
+    error: function(object, error) {
+      renderApp();
+      console.error('Fetch error:', error);
     }
   });
-
-  // Render the main app react component into the app div.
-  // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-  ReactDOM.render(pollForm, appDiv);
 
 })();
