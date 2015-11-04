5a26479b4e0d4c99450719a5528d346ac525158f
Adrien Joly <adrien@whyd.com>
2015-11-04 22:40:01 +0700

    converted PersistedPollForm.js -> jsx

---
+++ src/PersistedPollForm.jsx
@@ -0,0 +1,80 @@
+'use strict';
+
+var React = require('react');
+var PollForm = require('./PollForm.js');
+var itemStore = require('./itemStore.js');
+
+function getSelectedItems(form) {
+  var selected = [];
+  for (var i=0; i<form.elements.length; ++i) {
+    if (form.elements[i].name == 'selected' && form.elements[i].checked) {
+      selected.push(form.elements[i].value);
+    }
+  }
+  return selected;
+}
+
+module.exports = class PersistedPollForm extends React.Component {
+
+  getDefaultProps() {
+    return {
+      defaultItems: [],
+      setLoading: null,
+      form: null
+    };
+  }
+
+  getInitialState() {
+    return {
+      options: this.props.defaultItems
+    };
+  }
+
+  componentWillMount() {
+    console.log('Fetching options from Parse DB...');
+    var _this = this;
+    itemStore.fetchItems(function(error, items) {
+      _this.props.setLoading(false);
+      if (error) {
+        console.error('Fetch error:', error);
+      } else {
+        console.log('=>', items);
+        _this.setState({ options: items});
+      }
+    }, _this.props.defaultItems);
+  }
+
+  componentDidUpdate() {
+    this.props.onUpdate && this.props.onUpdate.call(this, arguments);
+  }
+
+  render() {
+    return <PollForm
+      ref='pollForm'
+      options={this.state.options}
+      onNewOption={this.onNewOption}
+      onValidSubmit={this.onValidSubmit} />;
+  }
+
+  onNewOption(newOption) {
+    this.setState({
+      options: this.state.options.concat([ newOption ])
+    });
+  }
+
+  onValidSubmit() {
+    var form = this.props.form;
+    var selectedItems = getSelectedItems(form);
+    this.refs.pollForm.setState({ disabled: true });
+    this.props.setLoading(true);
+    console.log('Saving new selected items...');
+    itemStore.syncItems(selectedItems, function() {
+      console.log('=>', arguments);
+      console.log('Subscribing to Mailchimp newsletter...');
+      form.submit(selectedItems);
+      // => will redirect to other page
+      // ... or what ? (TODO)
+    });
+  }
+
+}
